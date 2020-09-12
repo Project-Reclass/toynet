5ca45afad6691829fd1a1200c76ebe38d83a72dc
theo-bash <56284733+theo-bash@users.noreply.github.com>
2020-09-12 16:52:02 -0500

    Automated VPC with multiple subnets

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+# Terraform configuration
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+data "terraform_remote_state" "projectreclass-terraform" {
+  backend = "s3" 
+  config = {
+    bucket = "projectreclass-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
